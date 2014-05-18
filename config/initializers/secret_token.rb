43043ffbc692a835676d4c1377e9497b93af8dd6
Mike Larkin <mjlarkin@gmail.com>
2014-05-18 12:01:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Contestapp::Application.config.secret_key_base = 'f0cd2ba33857238bc79d936d366f11bfc422880ea6ab6d9d9995de004d4fbadbccdc4ecbf4d9f50f3e7353433a31062daf65b193ce3bffd97acc7007ae32872d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Contestapp::Application.config.session_store :cookie_store, key: '_contestapp_session'
